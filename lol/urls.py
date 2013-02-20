e75984a00ddc4a6c0cb5fc4502491d639c8a6226
Eric Betts <bettse@gmail.com>
2013-02-19 18:45:38 -0800

    correct how url in template was produced

--- lol/urls.py
+++ lol/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lol.views',
-    url(r'^$', 'index'),
-    url(r'^upload/$', 'upload'),
+    url(r'^$', 'index', name='index'),
+    url(r'^upload/$', 'upload', name='upload'),
 )
