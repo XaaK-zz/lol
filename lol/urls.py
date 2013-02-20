e53765b88df4a069c3b5608ec0dd299ff70f2eee
Eric Betts <bettse@gmail.com>
2013-02-19 19:20:02 -0800

    code for rating submission

--- lol/urls.py
+++ lol/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('lol.views',
     url(r'^$', 'index', name='index'),
     url(r'^upload/$', 'upload', name='upload'),
     url(r'^upload/new$', 'new', name='new'),
-    url(r'^rate/(?P<snippet_id>\d+)$', 'rate', name='rate'),
+    url(r'^rate/(?P<snippet_id>\d+)/$', 'rate', name='rate'),
 )
--- lol/views.py
+++ lol/views.py
@@ -6,18 +6,24 @@ from lol.models import Snippet, Language
 
 def index(request):
     random = Snippet.objects.order_by('?')[0]
-    return render_to_response('index.html', {'snippet': random})
+    return render_to_response('index.html', {'snippet': random}, context_instance=RequestContext(request))
 
 def upload(request):
     languages = Language.objects.all().order_by('name')
     return render_to_response('upload.html', {'languages': languages}, context_instance=RequestContext(request))
 
 def rate(request, snippet_id):
-    #log it
+    s = get_object_or_404(Snippet, pk=snippet_id)
+    bias = request.POST['submitButton']
+    if (bias == 'Leet'):
+        s.leet += 1
+    elif(bias == 'Lame'):
+        s.lame += 1
+    s.save()
     return redirect('index')
 
 def new(request):
-    lang = get_object_or_404(Language, pk=request.POST['language'])
+    lang = get_object_or_404(Language, pk=request.POST['language_id'])
     s = Snippet(code=request.POST['code'], language=lang)
     s.save()
     return redirect('index')
