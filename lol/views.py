36de0515b7f28826debbae958917288704e2d64e
Eric Betts <bettse@gmail.com>
2013-02-19 18:41:34 -0800

    add index view

--- lol/views.py
+++ lol/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+from lol.models import Snippet, Language
+
+def index(request):
+    random = Snippet.objects.order_by('?')[0]
+    return render_to_response('index.html', {'snippet': random})
+
+def upload(request):
+        return HttpResponse("Hello, world. You're at the upload page.")
