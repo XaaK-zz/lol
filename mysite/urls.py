0cfb864a0019443346dca74b80444b82742a4656
Eric Betts <bettse@gmail.com>
2013-02-19 18:25:34 -0800

    modularizing urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
 
+    url(r'^', include('lol.urls')),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
