5c87c7f9be55864d326829d0f3e4dfb284542c10
Eric Betts <bettse@gmail.com>
2013-02-20 22:56:19 -0800

    simplify upload logic

--- lol/urls.py
+++ lol/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('lol.views',
     url(r'^$', 'index', name='index'),
     url(r'^upload/$', 'upload', name='upload'),
-    url(r'^upload/new$', 'new', name='new'),
     url(r'^rate/(?P<snippet_id>\d+)/$', 'rate', name='rate'),
     url(r'^top/(?P<limit>\d+)/$', 'top', name='top'),
     url(r'^view/(?P<snippet_id>\d+)/$', 'view', name='view'),
--- lol/views.py
+++ lol/views.py
@@ -6,12 +6,11 @@ from lol.models import Snippet, Language
 import requests, json
 
 def index(request):
-    random = Snippet.objects.order_by('?')[0]
-    return render_to_response('index.html', {'snippet': random}, context_instance=RequestContext(request))
-
-def upload(request):
-    languages = Language.objects.all().order_by('name')
-    return render_to_response('upload.html', {'languages': languages}, context_instance=RequestContext(request))
+    if request.method == 'GET':
+        random = Snippet.objects.order_by('?')[0]
+        return render_to_response('index.html', {'snippet': random}, context_instance=RequestContext(request))
+    elif request.method == 'POST':
+        return redirect('index')
 
 def rate(request, snippet_id):
     s = get_object_or_404(Snippet, pk=snippet_id)
@@ -23,11 +22,16 @@ def rate(request, snippet_id):
     s.save()
     return redirect('index')
 
-def new(request):
-    lang = get_object_or_404(Language, pk=request.POST['language_id'])
-    s = Snippet(code=request.POST['code'], description=request.POST['description'], language=lang)
-    s.save()
-    return redirect('index')
+
+def upload(request):
+    if request.method == 'GET':
+        languages = Language.objects.all().order_by('name')
+        return render_to_response('upload.html', {'languages': languages}, context_instance=RequestContext(request))
+    elif request.method == 'POST':
+        lang = get_object_or_404(Language, pk=request.POST['language_id'])
+        s = Snippet(code=request.POST['code'], description=request.POST['description'], language=lang)
+        s.save()
+        return redirect('index')
 
 def top(request, limit):
     limit = int(limit)
