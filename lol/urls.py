6d174d4bbf61c14da4582fcface3f14500e4bc40
Eric Betts <bettse@gmail.com>
2013-02-21 10:22:57 -0800

    Integrate rate view into index view

--- lol/urls.py
+++ lol/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('lol.views',
     url(r'^$', 'index', name='index'),
     url(r'^upload/$', 'upload', name='upload'),
-    url(r'^rate/(?P<snippet_id>\d+)/$', 'rate', name='rate'),
     url(r'^top/(?P<limit>\d+)/$', 'top', name='top'),
     url(r'^view/(?P<snippet_id>\d+)/$', 'view', name='view'),
     url(r'^import_gist/$', 'import_gist', name='import_gist'),
--- lol/views.py
+++ lol/views.py
@@ -10,17 +10,15 @@ def index(request):
         random = Snippet.objects.order_by('?')[0]
         return render_to_response('index.html', {'snippet': random}, context_instance=RequestContext(request))
     elif request.method == 'POST':
+        bias = request.POST['submitButton']
+        snippet_id = request.POST['snippet_id']
+        s = get_object_or_404(Snippet, pk=snippet_id)
+        if (bias == 'Leet'):
+            s.leet += 1
+        elif(bias == 'Lame'):
+            s.lame += 1
+        s.save()
         return redirect('index')
-
-def rate(request, snippet_id):
-    s = get_object_or_404(Snippet, pk=snippet_id)
-    bias = request.POST['submitButton']
-    if (bias == 'Leet'):
-        s.leet += 1
-    elif(bias == 'Lame'):
-        s.lame += 1
-    s.save()
-    return redirect('index')
 
 
 def upload(request):
